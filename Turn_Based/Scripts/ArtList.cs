539fc2b7efb305eab05cf3281bcb35c50f42cce1
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2024-04-17 16:01:21 +0800

    Revert "Add Pegasus"
    This reverts commit e6739da53a3f15da2ce101f03c0422c8dec69fc3.

--- Turn_Based/Scripts/ArtList.cs
+++ Turn_Based/Scripts/ArtList.cs
@@ -2,5 +2,12 @@ public class ArtList {
     public  static string bars = "▀ █ ▄";
     public static string elepant = "1 	     ___      ___\n1 	    /   \\~~~~/   \\\n1    _ _ _ _(      o  o    )\n1   '        \\___      ___/\n1  /             (\\   |(\n1 /|              /\\  |\n1  \\   /____\\    /  \\ |\n1   |_|      |__|    \"\"\n";
     public static string twinDog = "1          __\n1       __/o \\_\n1       \\____  \\  \n1           /   \\\n1    __    //\\   \\\n1___/o \\--//--\\   \\_/\n1\\_____  ___   \\  |\n1      ||   \\ | \\ |\n1     _||   _|| _||";
+<<<<<<< Updated upstream
     public static string stickMan = "	   ( )\n	  /[ ]\\\n	 / [ ] \\\n  	   [ ]\n  	  /   \\\n 	 /     \\\n	=	=\n";
+=======
+    public static string stickMan = "1	   ( )\n	  /[ ]\\\n	 / [ ] \\\n  	   [ ]\n  	  /   \\\n 	 /     \\\n	=	=\n";
+    public static string spider = "1    \\  \\  ,,  /  /   \n1     '-.`\\()/`.-'   \n1    .--_'(  )'_--.   \n1   / /` /`\"\"`\\ `\\ \\      \n1    |  |  ><  |  |       \n1    \\  \\      /  /        \n1        '.__.'    ";
+    public static string hippo ="1      c~~p ,---------. \n1 ,---'oo  )           \\\n1( O O                  )/\n1 `=^='                 /\n1       \\    ,     .   /\n1       \\  |-----'|  /\n1       ||__|    |_|__|";
+
+>>>>>>> Stashed changes
 } 
